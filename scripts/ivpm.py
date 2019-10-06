e2a3ff976a6c5568eeaf9b23c5d524b7bbd7e199
Matthew Ballance <matt.ballance@gmail.com>
2019-10-06 15:31:01 -0400

    re-enable PATH modification

--- scripts/ivpm.py
+++ scripts/ivpm.py
@@ -102,7 +102,7 @@ def write_packages_mk(
     fh.write("  endif\n")
     fh.write("  IVPM_PYTHON_BINDIR := $(dir $(IVPM_PYTHON))\n")
 # Remove this until we can figure out what's going on
-#    fh.write("  PATH := $(IVPM_PYTHON_BINDIR):$(PATH)\n")
+    fh.write("  PATH := $(IVPM_PYTHON_BINDIR):$(PATH)\n")
     fh.write("  export PATH\n")
     fh.write("package_deps = " + project + "\n")
   
