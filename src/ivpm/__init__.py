512d5c2ea3a26369f4f11e278d6d64600ab39175
Matthew Ballance <matt.ballance@gmail.com>
2020-02-28 21:56:07 -0500

    Add utility for obtaining package version
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ src/ivpm/__init__.py
@@ -0,0 +1,24 @@
+
+def get_pkg_version(setup_py_path):
+    """Returns the package version based on the etc/ivpm.info file"""
+    rootdir = os.path.dirname(os.path.realpath(setup_py_path))
+
+    version=None
+    with open(os.path.join(rootdir, "etc", "ivpm.info"), "r") as fp:
+        while True:
+            l = fp.readline()
+            if l == "":
+                break
+            if l.find("version=") != -1:
+                version=l[l.find("=")+1:].strip()
+                break
+
+    if version is None:
+        raise Exception("Failed to find version in ivpm.info")
+
+    if "BUILD_NUM" in os.environ.keys():
+        version += "." + os.environ["BUILD_NUM"]
+
+    return version
+
+
