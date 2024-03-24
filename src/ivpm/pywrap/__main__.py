c7c36a85acefdaee3ea1c848bd209c8795d823dd
Matthew Ballance <matt.ballance@gmail.com>
2024-03-24 14:11:52 -0700

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/ivpm/pywrap/__main__.py
+++ src/ivpm/pywrap/__main__.py
@@ -6,6 +6,14 @@ import subprocess
 ps = ";" if platform.system() == "Windows" else ":"
 env = os.environ.copy()
 env["IVPM_PYTHONPATH"] = ps.join(sys.path)
+exec_dir = os.path.dirname(sys.executable)
+
+if platform.system() == "Windows":
+    # User scripts are in 'Scripts'
+    env["PATH"] = os.path.join(exec_dir, "Scripts") + ps + env["PATH"]
+else:
+    # User scripts are alongside the executable
+    env["PATH"] = exec_dir + ps + env["PATH"]
 
 def main():
     cmd = sys.argv[1:]
